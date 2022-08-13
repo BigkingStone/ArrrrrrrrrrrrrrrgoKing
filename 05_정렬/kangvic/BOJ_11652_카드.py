cfecac638810b4f3219d77937af99c3d34f4e7ef
kungYa <101095141+kangvic0615@users.noreply.github.com>
2022-08-13 16:08:19 +0900

    Update BOJ_11652_카드.py

--- 05_정렬/kangvic/BOJ_11652_카드.py
+++ 05_정렬/kangvic/BOJ_11652_카드.py
@@ -5,9 +5,9 @@ cnt = [0]*n
 for i in range(n):
     arr.append(int(input()))
 arr.sort()
-cnt = 0
-j = 0
-max_cnt = 2**62 + 1
+cnt = 0 # 현재 나온 수들의 횟수
+j = 0 # 가장 많이 나온 횟수
+max_cnt = 2**62 + 1 # 가장많이 나온 횟수의 숫자,,(가장 작은 값)
 for i in range(0,n):
     if i == n-1:
         if arr[i-1] == arr[i]:
@@ -21,10 +21,10 @@ for i in range(0,n):
         break
      #i가 n-1일때,, 그냥 continue하고 끝나버린다. 위의 경우를 확인해주어야 한다.
 
-    if arr[i-1] == arr[i] or i == 0:
+    if arr[i-1] == arr[i] or i == 0: #이전 값과 같으면 횟수 증가
         cnt += 1 
         continue
-    if j < cnt:
+    if j < cnt: #달라지면 이제 J, max_cnt를 업데이트 해준다.. 조건에 맞게
         j = cnt
         max_cnt = arr[i-1]
     elif j == cnt:
