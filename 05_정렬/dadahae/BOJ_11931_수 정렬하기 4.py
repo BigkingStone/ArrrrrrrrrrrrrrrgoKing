6a2f99878cda2ff0bfa7bbd8b19c4372eaa74b38
dadahae <50159740+dahae0320@users.noreply.github.com>
2022-08-13 14:46:05 +0900

    Create BOJ_11931_수 정렬하기 4.py

---
+++ 05_정렬/dadahae/BOJ_11931_수 정렬하기 4.py
@@ -0,0 +1,38 @@
+def merge_sort(st:int, en:int):
+  if en == st+1:
+    return
+  mid = (st+en) // 2
+  merge_sort(st,mid)
+  merge_sort(mid,en)
+  merge(st,en)
+
+def merge(st:int, en:int):
+  mid = (st+en) // 2
+  lidx = st
+  ridx = mid
+  for i in range(st, en):
+    if ridx == en:
+      temp[i] = arr[lidx]
+      lidx += 1
+    elif lidx == mid:
+      temp[i] = arr[ridx]
+      ridx += 1
+    elif arr[lidx] <= arr[ridx]:
+      temp[i] = arr[ridx]
+      ridx += 1
+    else:
+      temp[i] = arr[lidx]
+      lidx += 1
+  for i in range(st, en):
+    arr[i] = temp[i]
+
+arr = [0]*1000001
+temp = [0]*1000001
+
+N = int(input())
+for i in range(N):
+  arr[i] = int(input())
+
+merge_sort(0,N)
+
+print(" ".join(map(str, arr[:N])))
