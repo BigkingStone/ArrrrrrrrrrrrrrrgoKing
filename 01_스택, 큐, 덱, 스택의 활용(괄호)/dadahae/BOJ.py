f12e33fb16cfe5796fba75ecedda82b8e8843757
dadahae <50159740+dahae0320@users.noreply.github.com>
2023-03-13 23:05:52 +0900

    Update BOJ.py

--- 01_스택, 큐, 덱, 스택의 활용(괄호)/dadahae/BOJ.py
+++ 01_스택, 큐, 덱, 스택의 활용(괄호)/dadahae/BOJ.py
@@ -1,11 +1,1 @@
-def hanoi(a: int, b: int, n: int):
-  if n == 1:
-    print(a, b)
-    return
-  hanoi(a, 6-a-b, n-1)
-  print(a, b)
-  hanoi(6-a-b, b, n-1)
-
-N = int(input())
-print( 2**N - 1)
-hanoi(1, 3, N)
+# empty
